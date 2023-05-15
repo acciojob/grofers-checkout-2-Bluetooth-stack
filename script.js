0209e433c3f68ba58d9f700327ab7bcde573ffd5
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-05-15 13:40:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your code here
+const tab = document.querySelector('table');
+let prices = document.querySelectorAll('.price')
+console.log(prices);
+
+let numPrc = 0;
+for(let i = 0; i<prices.length; i++){
+  numPrc+=(parseInt(prices[i].innerText));
+}
+console.log(numPrc);
+
+const trow = document.createElement('tr');
+const data = document.createElement('td');
+data.colSpan = 2;
+data.setAttribute('data-ns-test', 'grandTotal');
+data.innerText = numPrc;
+trow.appendChild(data);
+tab.appendChild(trow);
